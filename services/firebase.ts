6671d8a383ed6913f4d1e9ccf7c1668c42163608
matheus-freedom <matheus1509beta@gmail.com>
2026-02-24 10:19:43 -0300

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'; // 💡 NOVO: Importando o Storage
 
 // Aqui o seu app puxa as chaves secretas que guardamos no Netlify
 const firebaseConfig = {
@@ -15,6 +16,7 @@ const firebaseConfig = {
 // Iniciando o "Prédio" do Firebase
 const app = initializeApp(firebaseConfig);
 
-// Abrindo as portas da Autenticação (Porteiro) e do Banco de Dados (Arquivos)
+// Abrindo as portas
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app); // 💡 NOVO: Exportando o cofre de imagens
